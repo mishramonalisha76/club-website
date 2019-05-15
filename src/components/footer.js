866040d5f15092435032e74c9796fc09bc4b3d3f
Monalisha Mishra <mishramonalisha76@gmail.com>
2019-05-15 20:38:56 +0530

    add contact component

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,3 +6,36 @@ const Footer = () => (
     <Sitemetadata />
   </div>
 );
+
+const Contact = () => (
+  <div className="contact">
+    <h1 className="contacttext">CONTACT US</h1>
+    <br />
+    <address className="address">
+      Kalinga Nagar,Ghatikia
+      <br />
+      CET, Bhubaneswar
+      <br />
+      Odisha
+    </address>
+    <span>
+      <i className="fa fa-envelope" />
+      <a href="">jgadgskls</a>
+    </span>
+    <span>
+      <i className="fa fa-phone" />
+      <a href="tel:1-847-555-5555">1-847-555-5555</a>
+    </span>
+    <span>
+      <a href="">
+        <i className="fa fa-facebook-f" />
+      </a>
+      <a href="">
+        <i className="fa fa-github" aria-hidden="true" />
+      </a>
+      <a href="">
+        <i className="fa fa-twitter" />
+      </a>
+    </span>
+  </div>
+);
